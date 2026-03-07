32277a2f91bd6ea1e0c15a796916fa4b1638e53b
Krishiv <krishiv248@gmail.com>
2026-03-07 15:02:26 +0530

    chore:adding basic file structure and new files

--- locaify/__main__.py
+++ locaify/__main__.py
@@ -1,5 +1,6 @@
 def main() -> None:
     print("Locaify 0.1.0 — starting...")
 
+
 if __name__ == "__main__":
     main()
---
+++ locaify/core/models.py
@@ -0,0 +1,5 @@
+"""
+This is the file where all the variable and all are define and it will be used to call and used as a common across the entire app
+"""
+
+from __future__ import annotations
